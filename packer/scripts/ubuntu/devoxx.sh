e370f7b8d3ab5efa3ce85dc19af405b5dd5d4822
Olivier Bazoud <olivier.bazoud@gmail.com>
2014-04-06 21:56:32 +0200

    Add more packages, Add jhipster war

--- packer/scripts/ubuntu/devoxx.sh
+++ packer/scripts/ubuntu/devoxx.sh
@@ -2,4 +2,7 @@
 
 apt-get install -y wget curl vim git tree zip
 
-apt-get install -y -d apache2 tomcat7 mysql-server
+apt-get install -y -d apache2 tomcat7 mysql-server mysql-client libmysqlclient-dev
+
+mkdir -p /opt/apps
+wget -O /opt/apps/jhipster.war http://dl.bintray.com/obazoud/generic/jhipster-0.1.war
