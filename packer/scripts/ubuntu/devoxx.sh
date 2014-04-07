d55506d6c2ad7415da98e4b56831b0905665e9ab
Olivier Bazoud <obazoud@kyriba.com>
2014-04-07 17:22:02 +0200

    Update jhipster

--- packer/scripts/ubuntu/devoxx.sh
+++ packer/scripts/ubuntu/devoxx.sh
@@ -5,4 +5,4 @@ apt-get install -y wget curl vim git tree zip
 apt-get install -y -d apache2 tomcat7 mysql-server mysql-client libmysqlclient-dev
 
 mkdir -p /opt/apps
-wget -O /opt/apps/jhipster.war http://dl.bintray.com/obazoud/generic/jhipster-0.1.war
+wget -O /opt/apps/jhipster.war http://dl.bintray.com/obazoud/generic/jhipster-0.3.war
